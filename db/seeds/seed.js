5895c55b2252d31166bea99072568862d0688fc0
hjaox <hectorjana.o@gmail.com>
2024-01-30 14:54:31 +0000

    [FEATURE] added projects table to DB

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,15 +3,18 @@ const db = require('../connection');
 
 function seed({usersData}) {
     return db
-    .query(`DROP TABLE IF EXISTS users`)
+    .query(`DROP TABLE IF EXISTS projects`)
+    .then(() => {
+        return db.query(`DROP TABLE IF EXISTS users`);
+    })    
     .then(() => {
         return db.query(`
-        CREATE TABLE users(
-            user_id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            username VARCHAR NOT NULL,
-            email VARCHAR NOT NULL
-        );`
+            CREATE TABLE users(
+                user_id SERIAL PRIMARY KEY,
+                name VARCHAR NOT NULL,
+                username VARCHAR NOT NULL,
+                email VARCHAR NOT NULL
+            );`
         );
     })
     .then(() => {
@@ -20,8 +23,22 @@ function seed({usersData}) {
             usersData.map(({name, username, email}) => [name, username, email])
         );
 
-        return db.query(insertUsersQueryStr)
+        return db.query(insertUsersQueryStr);
     })
+    .then(() => {
+        return createTableProjects();
+    })
+}
+
+//projects under a certain user
+function createTableProjects() {
+    return db
+    .query(`CREATE TABLE projects(
+        project_id SERIAL PRIMARY KEY,
+        project_name VARCHAR NOT NULL,
+        owner VARCHAR NOT NULL REFERENCES users(username)
+        );`
+    )
 }
 
 module.exports = seed;
